e231d3ea53538f1590a33181e66bb9e91bfd8b86
Bailey Dalton <baidalton@gmail.com>
2021-02-20 19:44:09 -0700

    fixed it

--- index.js
+++ index.js
@@ -1,51 +1,97 @@
-module.exports = function guessAge(number, boolean, year) {
-  console.log(
-    "number between 2 and 10:",
-    number,
-    "celebrated birthday this year:",
-    boolean,
-    "birthyear:",
-    year
-  );
+// module.exports = function guessAge(number, boolean, year) {
+//   console.log(
+//     "number between 2 and 10:",
+//     number,
+//     "celebrated birthday this year:",
+//     boolean,
+//     "birthyear:",
+//     year
+//   );
 
-  if (number > 10 || number < 2) {
-    console.log("number must be between 2 and 10");
-  } else {
-  }
+//   if (number > 10 || number < 2) {
+//     console.log("number must be between 2 and 10");
+//   } else {
+//   }
 
-  let numByTwo = number * 2;
+//   let numByTwo = number * 2;
 
-  let numPlusFive = numByTwo + 5;
+//   let numPlusFive = numByTwo + 5;
 
-  let numByFifty = numPlusFive * 50;
+//   let numByFifty = numPlusFive * 50;
 
-  if (boolean === true) {
-    nextNumber = numByFifty + 1771
-  }
-  if (boolean === false) {
-    nextNumber = numByFifty + 1770
-  }
+//   if (boolean === true) {
+//     nextNumber = numByFifty + 1771
+//   }
+//   if (boolean === false) {
+//     nextNumber = numByFifty + 1770
+//   }
 
 
-  let numberMinusYear = nextNumber - year;
+//   let numberMinusYear = nextNumber - year;
 
-  (ageArray = []), (string = numberMinusYear.toString());
+//   (ageArray = []), (string = numberMinusYear.toString());
 
-  for (var i = 0, len = string.length; i < len; i += 1) {
-    ageArray.push(+string.charAt(i));
-  }
+//   for (var i = 0, len = string.length; i < len; i += 1) {
+//     ageArray.push(+string.charAt(i));
+//   }
 
-  let newArray = [];
+//   let newArray = [];
 
-  newArray.push(ageArray[1]);
-  newArray.push(ageArray[2]);
+//   newArray.push(ageArray[1]);
+//   newArray.push(ageArray[2]);
 
-  let age = `${ageArray[1]}` + `${ageArray[2]}`;
+//   let age = `${ageArray[1]}` + `${ageArray[2]}`;
 
-  console.log("guessed age:", age);
+//   console.log("guessed age:", age);
 
-  return age
+//   return age
 
+// }
+
+function guessAge(number, boolean, year) {
+    console.log(
+      "number between 1 and 10:",
+      number,
+      "celebrated birthday this year:",
+      boolean,
+      "birthyear:",
+      year
+    );
+  
+    if (number > 10 || number < 2) {
+      console.log("number must be between 2 and 10");
+    } else {
+        let numByTwo = number * 2;
+  
+        let numPlusFive = numByTwo + 5;
+      
+        let numByFifty = numPlusFive * 50;
+      
+        if (boolean === true) {
+          nextNumber = numByFifty + 1771
+        }
+        if (boolean === false) {
+          nextNumber = numByFifty + 1770
+        }
+      
+      
+        let numberMinusYear = nextNumber - year;
+      
+        (ageArray = []), (string = numberMinusYear.toString());
+      
+        for (var i = 0, len = string.length; i < len; i += 1) {
+          ageArray.push(+string.charAt(i));
+        }
+      
+        let newArray = [];
+      
+        newArray.push(ageArray[1]);
+        newArray.push(ageArray[2]);
+      
+        let age = `${ageArray[1]}` + `${ageArray[2]}`;
+      
+        console.log("guessed age:", age);
+    }
 }
 
-
+guessAge(13, false, 1998)
